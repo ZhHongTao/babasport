2552124dedafcf8ddc3b8a5c2c3c0f4bebc1ecc6
zhang <zhang9736653132@qq.com>
2017-08-17 17:34:57 +0800

    修改了

--- babasport-common/src/main/java/com/itheima/git/demo/Demo.java
+++ babasport-common/src/main/java/com/itheima/git/demo/Demo.java
@@ -3,7 +3,7 @@ package com.itheima.git.demo;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("dafadf");
+		System.out.println("dafadffdafdasssss");
 		System.out.println("daffadf");
 	}
 }
