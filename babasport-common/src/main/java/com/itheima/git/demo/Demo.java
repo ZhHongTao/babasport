4578b71304d175c781c56a50d2d023a30bc7a073
zhang <zhang9736653132@qq.com>
2017-08-18 09:18:28 +0800

    实验

--- babasport-common/src/main/java/com/itheima/git/demo/Demo.java
+++ babasport-common/src/main/java/com/itheima/git/demo/Demo.java
@@ -3,9 +3,8 @@ package com.itheima.git.demo;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("dafadffdafdasssss");
-		System.out.println("daffadf");
-		System.out.println("dafdasfdf");
-		System.out.println("dfafadf");
+		String a = new String("aa");
+		String b = new String("aa");
+		System.out.println(a.equals(b));
 	}
 }
