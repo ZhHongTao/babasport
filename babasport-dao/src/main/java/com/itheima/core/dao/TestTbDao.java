f12cfad7b4f6e08b3329c530a5d7fbd2e6309292
zhang <zhang9736653132@qq.com>
2017-08-18 19:28:49 +0800

    环境好了

---
+++ babasport-dao/src/main/java/com/itheima/core/dao/TestTbDao.java
@@ -0,0 +1,8 @@
+package com.itheima.core.dao;
+
+import com.itheima.core.pojo.TestTb;
+
+public interface TestTbDao {
+
+	public void addTestTb(TestTb testTb);
+}
---
+++ babasport-interferce/src/main/java/com/itheima/core/service/TestTbService.java
@@ -0,0 +1,8 @@
+package com.itheima.core.service;
+
+import com.itheima.core.pojo.TestTb;
+
+public interface TestTbService {
+
+	public void addTestTb(TestTb testTb);
+}
